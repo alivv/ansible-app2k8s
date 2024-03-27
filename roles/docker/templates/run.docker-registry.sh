33504729c219c213ca968943da58a317fcaa72e0
Elvin <hi@elvin.vip>
2024-03-27 18:36:05 +0800

    update for docker-registry

--- roles/docker/templates/run.docker-registry.sh
+++ roles/docker/templates/run.docker-registry.sh
@@ -1,33 +1,69 @@
 #!/bin/bash
-# run.docker-registry.sh
-# deploy docker registry +ui
-# deploy by ansible {{ ansible_date_time.date }}
-# By Elvin 
+#run.docker-registry.ui.sh
+#docker-registry +ui
+# By Elvin , update 2024-3-27
 
 ########## conifg
 
 #data directory
 data_dir={{ registry_data_dir }}
-mkdir -p $data_dir
+mkdir -p $data_dir/conf
 cd $data_dir
 
 #https cert
 domain_name={{ registry_domain_name }}
 download_url={{ registry_download_url }}
-if [  ! -e ${domain_name}.key -o  ! -e ${domain_name}.pem ];then
-curl -so ${domain_name}.key    ${download_url}/${domain_name}.key
-curl -so ${domain_name}.pem ${download_url}/${domain_name}.pem
+if [  ! -f conf/${domain_name}.key -o  ! -f conf/${domain_name}.pem ];then
+curl -so conf/${domain_name}.key    ${download_url}/${domain_name}.key
+curl -so conf/${domain_name}.pem ${download_url}/${domain_name}.pem
 fi
 
 #create passworld file, user {{ registry_username }} passworld {{ registry_password }} 
-docker run --rm registry.cn-hangzhou.aliyuncs.com/elvin/htpasswd {{ registry_username }} {{ registry_password }} >htpasswd
-#alivv/htpasswd
-
+if [ ! -f conf/htpasswd ];then
+docker run --rm registry.aliyuncs.com/elvin/htpasswd {{ registry_username }} {{ registry_password }} >conf/htpasswd
+fi
 
 #Create Network {{ registry_net_name }}
 if [ $(docker network ls |grep {{ registry_net_name }} |wc -l) -ne 1 ];then
     docker network create --subnet 10.20.20.0/24 --gateway 10.20.20.1 {{ registry_net_name }}
 fi
+
+#https
+#docker exec -it registry-ui cat /etc/nginx/conf.d/default.conf |egrep -v "#"
+cat > conf/https.conf << EOF
+server {
+    listen 80;
+    listen              443 ssl;
+    server_name         _;
+    ssl_protocols       TLSv1.1 TLSv1.2 TLSv1.3;
+    ssl_ciphers         EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;
+    ssl_certificate     /etc/nginx/conf.d/${domain_name}.pem;
+    ssl_certificate_key /etc/nginx/conf.d/${domain_name}.key;
+    root   /usr/share/nginx/html;
+
+    client_max_body_size 0;
+    chunked_transfer_encoding on;
+    proxy_ssl_server_name on;
+    proxy_buffering off;
+    proxy_request_buffering off; 
+    proxy_ignore_headers "X-Accel-Buffering";
+
+    location /v2 {
+        if (\$http_user_agent ~ "^(docker\/1\.(3|4|5(?!\.[0-9]-dev))|Go ).*$" ) {
+            return 404;
+        }
+        proxy_http_version 1.1;
+        proxy_pass http://registry-srv;
+    }
+
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+}
+EOF
+
+chown 101 -R conf
 
 ########## container
 
@@ -35,34 +71,34 @@ fi
 docker rm -f registry-srv &>/dev/null
 docker run -dit --name registry-srv \
 --privileged=true --restart=always \
---net {{ registry_net_name }} -p 443:443 \
---memory 512M \
+--net {{ registry_net_name }} \
+--memory 4096M \
 -v $data_dir:/var/lib/registry \
 -v /etc/localtime:/etc/localtime:ro \
 -e REGISTRY_AUTH=htpasswd \
 -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
--e REGISTRY_AUTH_HTPASSWD_PATH=/var/lib/registry/htpasswd \
--e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+-e REGISTRY_AUTH_HTPASSWD_PATH=/var/lib/registry/conf/htpasswd \
+-e REGISTRY_HTTP_ADDR=0.0.0.0:80 \
 -e REGISTRY_STORAGE_DELETE_ENABLED=true \
--e REGISTRY_HTTP_TLS_KEY=/var/lib/registry/${domain_name}.key \
--e REGISTRY_HTTP_TLS_CERTIFICATE=/var/lib/registry/${domain_name}.pem \
-registry.cn-hangzhou.aliyuncs.com/elvin/registry:latest
-# registry:latest
+-e REGISTRY_HTTP_HEADERS_Access-Control-Allow-Origin="[https://${domain_name}]" \
+registry.aliyuncs.com/elvin/registry:2.8
 
 #registry-ui
 docker rm -f registry-ui  &>/dev/null
 docker run -d --name registry-ui \
 --restart=always \
---memory 64M \
---net {{ registry_net_name }} -p {{ registry_ui_port }}:80 \
+--memory 256M \
+--net {{ registry_net_name }} -p 80:80 -p 443:443 \
 -v /etc/localtime:/etc/localtime:ro \
--e REGISTRY_URL=https://registry-srv:443 \
+-v $data_dir/conf:/etc/nginx/conf.d \
 -e PULL_URL=${domain_name} \
 -e DELETE_IMAGES=true \
 -e REGISTRY_TITLE="Docker registry" \
-registry.cn-hangzhou.aliyuncs.com/elvin/docker-registry-ui:1.5-static
-# joxit/docker-registry-ui:1.5-static
-
+-e NGINX_PROXY_PASS_URL=http://registry-srv \
+-e SINGLE_REGISTRY=true \
+-e SHOW_CATALOG_NB_TAGS=true \
+-e REGISTRY_SECURED=true \
+registry.aliyuncs.com/elvin/docker-registry-ui:2
 
 ########## 
 
